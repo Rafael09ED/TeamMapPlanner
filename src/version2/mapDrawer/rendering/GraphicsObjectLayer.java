925dc7462e8b2842fb3bbe02698613c6bb515183
Rafael09ED <starmarstrek@gmail.com>
2015-03-30 19:57:47 -0400

    Rendering Optimization Done
    Everything is only re-rendered if an object is removed, otherwise it is
    just added to the previous frame.

--- src/version2/mapDrawer/rendering/GraphicsObjectLayer.java
+++ src/version2/mapDrawer/rendering/GraphicsObjectLayer.java
@@ -4,48 +4,104 @@ import version2.mapDrawer.graphicsObjects.GraphicsObject;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.*;
 import java.util.List;
 
 /**
  * Created by ADMIN on 3/30/2015.
  */
 public class GraphicsObjectLayer {
-    private boolean hasLayerChanged = false;
+    private boolean requiresRender = false;
     private List<GraphicsObject> graphicsObjects;
     private BufferedImage layerPrecomposed;
     private Point maxPoint;
 
+    private boolean objectAdded = false;
+    private Point maxPointAdded;
+    private List<GraphicsObject> objectsAdded;
+
     public GraphicsObjectLayer() {
         graphicsObjects = Collections.synchronizedList(new ArrayList<GraphicsObject>());
-        layerPrecomposed = new BufferedImage(1,1, BufferedImage.TYPE_INT_ARGB);
+        layerPrecomposed = new BufferedImage(1, 1, BufferedImage.TYPE_INT_ARGB);
+
+        objectsAdded = new LinkedList<GraphicsObject>();
+        maxPoint = new Point(1, 1);
+        maxPointAdded = new Point(1,1);
     }
-    public void addGraphicsObject(GraphicsObject graphicsObjectIn){
+
+    public void addGraphicsObject(GraphicsObject graphicsObjectIn) {
         graphicsObjects.add(graphicsObjectIn);
-        maxPoint = solveSize(maxPoint, graphicsObjectIn.getMaxPoint());
-        hasLayerChanged = true;
+        
+        objectsAdded.add(graphicsObjectIn);
+        maxPointAdded = solveSize(maxPointAdded, graphicsObjectIn.getMaxPoint());
+        objectAdded = true;
+    }
+
+    private void setRenderQuality(Graphics g){
+        Graphics2D g1 = (Graphics2D) g;
+        g1.setRenderingHint
+                (RenderingHints.KEY_ANTIALIASING,
+                        RenderingHints.VALUE_ANTIALIAS_ON);
+        g1.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
+                RenderingHints.VALUE_INTERPOLATION_BICUBIC);
 
     }
-    private Point solveSize(Point p1, Point p2){
-        return new Point((int) Math.max(p1.getX(), p2.getX()),(int) Math.max(p1.getX(),p2.getX()) );
+
+    private Point solveSize(Point p1, Point p2) {
+        return new Point((int) Math.max(p1.getX(), p2.getX()) + 1, (int) Math.max(p1.getX(), p2.getX()) + 1);
     }
 
     public boolean removeGraphicsObjects(GraphicsObject graphicsObjectIn) {
-        return graphicsObjects.remove(graphicsObjectIn);
+        boolean removed = graphicsObjects.remove(graphicsObjectIn);
+        if (removed) {
+            requiresRender = true;
+        }
+        return removed;
     }
 
     public void render(Graphics g) {
 
-        if (hasLayerChanged){
-            layerPrecomposed = new BufferedImage((int) maxPoint.getX(),(int) maxPoint.getY(), BufferedImage.TYPE_INT_ARGB);
+        if (requiresRender) {
+            layerPrecomposed = new BufferedImage((int) maxPoint.getX(), (int) maxPoint.getY(), BufferedImage.TYPE_INT_ARGB);
             Graphics g2 = layerPrecomposed.getGraphics();
+            setRenderQuality(g2);
+
             for (GraphicsObject graphicsObject : graphicsObjects) {
-
+                graphicsObject.paint(g2);
             }
+        } else if (objectAdded){
+            Point tempMaxPoint = solveSize(maxPoint, maxPointAdded);
 
-            hasLayerChanged = false;
+            if ( maxPoint.equals(tempMaxPoint) ) {
+                Graphics g1 = layerPrecomposed.getGraphics();
+                setRenderQuality(g1);
+
+                for (Iterator<GraphicsObject> iterator = objectsAdded.iterator(); iterator.hasNext(); ) {
+                    iterator.next().paint(g1);
+                    iterator.remove();
+                }
+
+            } else {
+                maxPoint = tempMaxPoint;
+
+                BufferedImage tempLayer = new BufferedImage((int) maxPoint.getX(), (int) maxPoint.getY(), BufferedImage.TYPE_INT_ARGB);
+                Graphics g1 = tempLayer.getGraphics();
+                g1.drawImage(layerPrecomposed, 0, 0, null);
+
+                for (Iterator<GraphicsObject> iterator = objectsAdded.iterator(); iterator.hasNext(); ) {
+                    iterator.next().paint(g1);
+                    iterator.remove();
+                }
+
+                g1.dispose();
+                layerPrecomposed = tempLayer;
+            }
         }
+        // sets render tracker
+        requiresRender = false;
+        maxPointAdded = new Point(1,1);
+        objectAdded = false;
+
         g.drawImage(layerPrecomposed, 0, 0, null);
     }
 }
