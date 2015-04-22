1af0c2da7ffce00bf2b282f299338aaf41ecc124
Rafael09ED <starmarstrek@gmail.com>
2015-04-22 08:09:54 -0400

    Version 5 src cleanup + Updated Plan

--- src/version4/SETTINGS/NAMING_SETTINGS.java
+++
@@ -1,9 +0,0 @@
-package version4.SETTINGS;
-
-/**
- * Created by Rafael on 4/18/2015.
- */
-public class NAMING_SETTINGS {
-    public static String rootFolderName = "Canvas";
-    public static String untitledName = "Untitled";
-}
--- src/version4/mapDrawer/rendering/tools/IRenderable.java
+++
@@ -1,8 +0,0 @@
-package version4.mapDrawer.rendering.tools;
-
-/**
- * Created by Rafael on 4/3/2015.
- */
-public interface IRenderable {
-    public void render(RenderingInterface renderingInterface);
-}
