71c882f5cdba69487d55b461ff0b127dabdec8a9
Rafael09ED <starmarstrek@gmail.com>
2015-04-30 22:38:20 -0400

    Version 5.0.1.0.2 - Render Un-Op. Hotfix
    Just replace the render optimizer with a rendering system that renders
    everything at once. This way I can come back to it and work on other
    things until then

--- src/version5/mapDrawer/rendering/RenderingWrapper.java
+++ src/version5/mapDrawer/rendering/RenderingWrapper.java
@@ -1,10 +1,16 @@
 package version5.mapDrawer.rendering;
 
+import version5.mapDrawer.itemManagement.infoTypes.BoundingBox2D;
 import version5.mapDrawer.itemManagement.itemTracker.canvasGroups.CanvasGroupFolder;
 import version5.mapDrawer.itemManagement.itemTracker.canvasGroups.CanvasGroupLayer;
+import version5.mapDrawer.itemManagement.itemTracker.canvasItems.CanvasItem;
 import version5.mapDrawer.rendering.optimization.CanvasGroupRenderOptimizer;
 import version5.mapDrawer.rendering.optimization.RenderData;
 
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 /**
@@ -16,12 +22,42 @@ public class RenderingWrapper {
     private Set<CanvasGroupLayer> changedLayers;
 
     public RenderingWrapper() {
-         canvasGroupRenderOptimizer = new CanvasGroupRenderOptimizer(this);
+        canvasGroupRenderOptimizer = new CanvasGroupRenderOptimizer(this);
     }
 
     public RenderData getRender(CanvasGroupFolder canvasGroupFolder) {
+        if (true) {
+            List<CanvasGroupLayer> canvasGroupLayers = new ArrayList<>();
+            canvasGroupFolder.getAllSubLayersOrdered(canvasGroupLayers);
+
+            BoundingBox2D boundingBox2D = new BoundingBox2D();
+            for (CanvasGroupLayer canvasGroupLayer : canvasGroupLayers) {
+                boundingBox2D.addBoundingBox(canvasGroupLayer.getBoundingBox());
+            }
+
+            BufferedImage bufferedImage = new BufferedImage(
+                    boundingBox2D.getIntBoxWidth(),
+                    boundingBox2D.getIntBoxHeight(),
+                    BufferedImage.TYPE_INT_ARGB
+            );
+            for (CanvasGroupLayer canvasGroupLayer : canvasGroupLayers) {
+
+
+                Graphics2DRenderer graphics2DRenderer = new Graphics2DRenderer();
+
+
+
+                graphics2DRenderer.setGraphicsToRenderTo((Graphics2D) bufferedImage.getGraphics());
+
+                for (CanvasItem canvasItem : canvasGroupLayer.getCanvasItems()) {
+                    canvasItem.render(graphics2DRenderer);
+                }
+            }
+            return new RenderData(bufferedImage,boundingBox2D);
+        }
         return canvasGroupRenderOptimizer.getRender(canvasGroupFolder);
     }
+
     public RenderData getRender(CanvasGroupLayer canvasGroupLayer) {
         return canvasGroupRenderOptimizer.getRender(canvasGroupLayer);
     }
